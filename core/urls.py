89179d639d875a2ed0a44a6fbbd4d6b23f95b25f
Welley <monteirowelley7@gmail.com>
2023-02-11 18:40:10 -0300

    refatorando o cod

--- core/urls.py
+++ core/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('receitas/', include('receitas.urls')),
+    path('', include('receitas.urls')),
 ]
