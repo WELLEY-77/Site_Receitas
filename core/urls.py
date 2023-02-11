bce0b21d17ab8f37906981541e8e1e9ddbe21e6e
Welley <monteirowelley7@gmail.com>
2023-02-11 18:13:23 -0300

    aad nome da rota

--- core/urls.py
+++ core/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('receitas.urls')),
+    path('receitas/', include('receitas.urls')),
 ]
