a34d132fb92aaff10445b0a8757cfbccf69e3195
Welley <monteirowelley7@gmail.com>
2023-02-11 18:30:45 -0300

    cirando a funcao para a rota

--- receitas/views.py
+++ receitas/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def receita(request):
     return render(request, 'receitas/index.html')
+
+
+def imagem(request):
+    return render(request, 'receitas/receita.html')
