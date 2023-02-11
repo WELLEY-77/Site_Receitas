fe919b7645b1601359852f5499c1ef1d4c526b0e
Welley <monteirowelley7@gmail.com>
2023-02-11 18:29:53 -0300

    add nova rotas

--- receitas/urls.py
+++ receitas/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from. import views
 
 urlpatterns = [
-    path('',views.receita )
+    path('',views.receita , name='receita'),
+    path('imagem/', views.imagem, name='imagem')
 ]
