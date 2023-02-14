ab079980d02d4b4d52c188b5d9aca5c25d60af68
Welley <monteirowelley7@gmail.com>
2023-02-14 15:23:24 -0300

    apresentando a pasta criada para o core

--- core/settings.py
+++ core/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'templates', BASE_DIR / 'base_templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
