aa1b1a46439eed5e2f9cd0ecb5f23d73e193d9d4
Welley <monteirowelley7@gmail.com>
2023-02-21 14:51:02 -0300

    criaçao de um banco de dados

--- core/settings.py
+++ core/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'app_receita',
+        'USER':'postgres',
+        'PASSWORD':'welley2002',
+        'HOST':'localhost',
     }
 }
 
