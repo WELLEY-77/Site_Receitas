a9877e9dcc0b318c3f21d622239f3902a4d93c79
Welley <monteirowelley7@gmail.com>
2023-02-21 13:51:52 -0300

    refatoraçao do codigo

--- receitas/views.py
+++ receitas/views.py
@@ -2,7 +2,19 @@ from django.shortcuts import render
 
 
 def receita(request):
-    return render(request, 'receitas/index.html')
+
+    receitas = {
+        1:'Sopa de legumes',
+        2:'Sorvete',
+        3:'Lasanha',
+        4:'Bolo de chocolate',
+    }
+
+    dados = {
+        'nomes_da_receita':receitas,
+    }
+
+    return render(request, 'receitas/index.html', dados)
 
 
 def imagem(request):
