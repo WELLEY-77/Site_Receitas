d8ffe45a2e718ed5483eed08093059b1c040202e
Welley <monteirowelley7@gmail.com>
2023-02-24 15:31:47 -0300

    colocando os object do banco de dados em uma variavel

--- receitas/views.py
+++ receitas/views.py
@@ -4,12 +4,10 @@ from .models import Receita
 
 def receita(request):
 
-    receitas = {
-        Receita.objects.all()
-    }
-
+    receitas = Receita.objects.all()
+                 
     dados = {
-        'receita':receitas,
+        'receitas':receitas,
     }
 
     return render(request, 'receitas/index.html', dados)
