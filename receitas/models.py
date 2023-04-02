f4efba32f2cfe38f3c58f2ddc3e79b8ffa0b382e
Welley <monteirowelley7@gmail.com>
2023-04-02 15:22:01 -0300

    alterando o models, e reformulando o admin

--- receitas/models.py
+++ receitas/models.py
@@ -2,10 +2,10 @@ from django.db import models
 from datetime import datetime
 
 class Receita(models.Model):
-    nome_receita = models.CharField(max_length=100)
-    ingredientes = models.TextField()
-    modo_preparo = models.TextField()
-    tempo_preparo = models.IntegerField()
-    rendimento = models.CharField(max_length=100)
-    categoria = models.CharField(max_length=100)
-    date_receita = models.DateTimeField(default=datetime.now, blank=True)
+    nome_receita = models.CharField('Nome da Receita', max_length=100)
+    ingredientes = models.TextField('Igredientes')
+    modo_preparo = models.TextField('Modo de Preparo')
+    tempo_preparo = models.IntegerField('Tempo de Preparo')
+    rendimento = models.CharField('Remdimento', max_length=100)
+    categoria = models.CharField('Categoria', max_length=100)
+    date_receita = models.DateTimeField(' Data da Receita ', default=datetime.now, blank=True)
