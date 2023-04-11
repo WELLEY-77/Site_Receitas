589056521c0f7b9c7b708f63a7bf62e31d403d46
Welley <monteirowelley7@gmail.com>
2023-04-11 15:43:18 -0300

    cirando templates, reorganizando as pastas e nomeclaturas

--- core/urls.py
+++ core/urls.py
@@ -7,5 +7,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('receitas.urls')),
-    path('', include('usuarios.urls'))
+    path('usuarios/', include('usuarios.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
