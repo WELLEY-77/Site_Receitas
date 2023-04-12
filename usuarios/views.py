d557886852ea8e62ed26e2334d9bd1be0868f07d
Welley <monteirowelley7@gmail.com>
2023-04-12 14:24:44 -0300

    fazendo requisiçoes no method post, e autenticação de usuario

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,7 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
 
 def cadastro(request):
-    return render(request ,'usuarios/cadastro.html')
+
+    if request.method == 'POST':
+        nome = request.POST['nome']
+        email = request.POST['email']
+        senha = request.POST['password']
+        senha2 = request.POST['password2']
+
+        if not nome.strip():
+            print('Campo nome nao pode ficar em branco')
+            return redirect('cadastro')
+        
+        if not email.strip():
+            print('Campo nome nao pode ficar em branco')
+            return redirect('cadastro')
+        
+        if senha != senha2:
+            print('Senha incorreta')
+            return redirect('cadastro')
+
+        if User.objects.filter(email=email).exists():
+            print('Usuario ja cadastrado')
+            return redirect('cadastro')
+        
+        user = User.objects.create_user(username=nome, email=email, password=senha2)
+        user.save()
+        
+        print('Usuario cadastrado com sucesso')
+
+        return redirect('login')
+    else:
+        return render(request ,'usuarios/cadastro.html')
 
 def login(request):
     return render(request, 'usuarios/login.html')
